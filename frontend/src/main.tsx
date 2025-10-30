d82697268eafc8ec4b7843d5686c695083719c67
Vijay <kumarvijayy036@gmail.com>
2025-10-30 09:24:27 +0530

    add scripts

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,21 @@
 import { StrictMode } from "react";
+// For React 19, use this import:
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById("root")!).render(
+// Add type assertion for the root element
+const rootElement = document.getElementById("root") as HTMLElement;
+
+if (!rootElement) {
+  throw new Error("Root element not found");
+}
+
+createRoot(rootElement).render(
   <StrictMode>
     <BrowserRouter>
-    <App /></BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>
 );
